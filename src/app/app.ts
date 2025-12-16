95a86316d566c707a1f604046cbe33a759bcc174
Paul Gross <p.gross6408@gmail.com>
2025-12-16 17:50:46 +0100

    feat: add skills section with content and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './sections/hero/hero';
+import { Skills } from './sections/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './skills.scss',
 })
 export class Skills {
-
+  skills = [
+    { name: 'Angular', icon: 'assets/icons/angular.png' },
+    { name: 'TypeScript', icon: 'assets/icons/ts.png' },
+    { name: 'JavaScript', icon: 'assets/icons/js.png' },
+    { name: 'HTML', icon: 'assets/icons/html.png' },
+    { name: 'CSS', icon: 'assets/icons/css.png' },
+    { name: 'Git', icon: 'assets/icons/git.png' },
+    { name: 'Firebase', icon: 'assets/icons/firebase.png' },
+    { name: 'REST API', icon: 'assets/icons/rest-api.png' }
+  ];
 }
